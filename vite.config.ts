a21f054ff6ec92dd2dc1d6ff38e882c2b6ded69d
Javad Yousefi <javadyousefi9000@gmail.com>
2024-06-14 21:50:55 +0330

    add pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'node:url'
+import { VitePWA } from 'vite-plugin-pwa';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,6 +12,40 @@ export default defineConfig(({ mode }) => {
     },
     plugins: [
       react(),
+      // basicSsl(),
+      VitePWA({ 
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        manifest: {
+          name: 'Vite PWA Project',
+          short_name: 'Vite PWA Project',
+          theme_color: '#ffffff',
+          icons: [
+              {
+                  src: 'pwa-64x64.png',
+                  sizes: '64x64',
+                  type: 'image/png'
+              },
+              {
+                  src: 'pwa-192x192.png',
+                  sizes: '192x192',
+                  type: 'image/png'
+              },
+              {
+                  src: 'pwa-512x512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+                  purpose: 'any'
+              },
+              {
+                  src: 'maskable-icon-512x512.png',
+                  sizes: '512x512',
+                  type: 'image/png',
+                  purpose: 'maskable'
+              }
+          ],
+        }, 
+      })
     ],
     resolve: {
       alias: {
